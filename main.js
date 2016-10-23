3cc5c0898bd041760508d83aa962a3b2175a0a07
Katarzyna Watroba <katarzyna.liver@gmail.com>
2016-10-23 21:05:48 +0100

    used handlebars as templating engine

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var server = express();
+var exphbs  = require('express-handlebars');
 
-// server.get('/', function (request, response) {
-//     response.send('hello hello');
-// });
+server.engine('handlebars', exphbs({defaultLayout: 'main'}));
+server.set('view engine', 'handlebars');
 
 server.use(express.static('static'));
+
+server.get('/', function (req, res) {
+  res.render('home');
+});
+
+server.get('/przepisy', function (req, res) {
+  res.render('przepisy');
+});
 
 
 server.listen(80);
